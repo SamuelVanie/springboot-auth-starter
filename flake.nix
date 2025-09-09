8802b428749fbcac0fc3cdc5f832040fb380b4ba
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-09-09 14:40:49 -0400

    fix: remove extra parenthesis in flake file

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: {
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -37,7 +37,5 @@
               # Environment variables for development
               JAVA_HOME = "${pkgs.openjdk17}";
             };
-          }
-    );
-  };
+          });
 }
